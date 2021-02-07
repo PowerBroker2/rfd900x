7563c70990eb7cc4b098a6961782135334fab083
PB2 <gitstuff2@gmail.com>
2021-02-07 01:19:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name             = 'pySerialTransfer',
-    packages         = ['pySerialTransfer'],
-    version          = '2.1.1',
-    description      = 'Python package used to transmit and receive low overhead byte packets - especially useful for PC<-->Arduino USB communication (compatible with https://github.com/PowerBroker2/SerialTransfer)',
+    name             = 'rfd900x',
+    packages         = ['rfd900x'],
+    version          = '0.0.1',
+    description      = 'Python module for configuring RFD900x series radios',
     long_description = long_description,
     long_description_content_type = "text/markdown",
     author           = 'Power_Broker',
     author_email     = 'gitstuff2@gmail.com',
-    url              = 'https://github.com/PowerBroker2/pySerialTransfer',
-    download_url     = 'https://github.com/PowerBroker2/pySerialTransfer/archive/2.1.1.tar.gz',
-    keywords         = ['Arduino', 'serial', 'usb', 'protocol', 'communication'],
+    url              = 'https://github.com/PowerBroker2/rfd900x',
+    download_url     = 'https://github.com/PowerBroker2/rfd900x/archive/0.0.1.tar.gz',
+    keywords         = ['RFD', 'RFD-900', 'RFD-900x', 'RFD-900+', 'RFD 900', 'RFD 900x', 'RFD 900+'],
     classifiers      = [],
-    install_requires = ['pyserial']
+    install_requires = []
 )
