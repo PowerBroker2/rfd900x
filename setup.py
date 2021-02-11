f9e2066211d29dfca36d0fb57ea40b29d9b547ad
PB2 <gitstuff2@gmail.com>
2021-02-10 19:20:58 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
 setup(
     name             = 'rfd900x',
     packages         = ['rfd900x'],
-    version          = '0.0.5',
+    version          = '0.0.6',
     description      = 'Python module for configuring RFD900x series radios',
     long_description = long_description,
     long_description_content_type = "text/markdown",
     author           = 'Power_Broker',
     author_email     = 'gitstuff2@gmail.com',
     url              = 'https://github.com/PowerBroker2/rfd900x',
-    download_url     = 'https://github.com/PowerBroker2/rfd900x/archive/0.0.5.tar.gz',
+    download_url     = 'https://github.com/PowerBroker2/rfd900x/archive/0.0.6.tar.gz',
     keywords         = ['RFD', 'RFD-900', 'RFD-900x', 'RFD-900+', 'RFD 900', 'RFD 900x', 'RFD 900+'],
     classifiers      = [],
     install_requires = ['pyserial']
